7677bef7dc04962976739bfa919df625a27c83ae
Malau <malaury.lemaitresalmon@gmail.com>
2018-12-29 19:06:20 +0100

    Ajout des constantes

---
+++ constantes.py
@@ -0,0 +1,16 @@
+
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+titre_fenetre = "Labyrinthe MacGyver"
+image_icone = "Ressources/MacGyver.png"
+
+image_accueil = "Ressources/accueil.png"
+image_depart = "Ressources/depart.png"
+image_fond = "Ressources/fond.png"
+image_mac_gyver = "Ressources/MacGyver.png"
+image_arrivee = "Ressources/Gardien.png"
+image_mur = "Ressources/murs.png"
+image_avatar = "Ressources/MacGyver.png"
+
