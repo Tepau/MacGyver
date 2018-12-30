64778964e771c164eac3a7a31987af4c09f869ab
Malau <malaury.lemaitresalmon@gmail.com>
2018-12-30 14:12:08 +0100

    Modif de constantes

--- constantes.py
+++ constantes.py
@@ -1,4 +1,3 @@
-
 nombre_sprite_cote = 15
 taille_sprite = 30
 cote_fenetre = nombre_sprite_cote * taille_sprite
@@ -13,4 +12,6 @@ image_mac_gyver = "Ressources/MacGyver.png"
 image_arrivee = "Ressources/Gardien.png"
 image_mur = "Ressources/murs.png"
 image_avatar = "Ressources/MacGyver.png"
-
+image_aiguille = "Ressources/aiguille1.png"
+image_ether = "Ressources/ether1.png"
+image_tube = "Ressources/tube_plastique1.png"
