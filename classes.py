2079600fc7b2853322867c2d7f4ec12aa4489161
Malau <malaury.lemaitresalmon@gmail.com>
2019-01-02 21:22:24 +0100

    MAJ classes

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from constantes import *
+import random
 
 class Niveau: #Classe permettant de définir le niveau
 	
@@ -24,6 +25,7 @@ class Niveau: #Classe permettant de définir le niveau
 	def afficher(self, fenetre):
 		mur = pygame.image.load(image_mur).convert()
 		arrivee = pygame.image.load(image_arrivee).convert_alpha()
+		
 
 		num_ligne = 0
 		for ligne in self.structure:
@@ -35,25 +37,26 @@ class Niveau: #Classe permettant de définir le niveau
 					fenetre.blit(mur, (x,y))
 				if sprite == "a":
 					fenetre.blit(arrivee, (x,y))
+				
 				num_case += 1
 			num_ligne += 1
 
 
 class Perso:
 
-	def __init__(self, avatar, position):
+	def __init__(self, avatar, niveau):
 		self.avatar = pygame.image.load(image_avatar).convert_alpha()
 		self.x = 0
-		self.y = 0
+		self.y = 30
 		self.case_x = 0
-		self.case_y = 0
-		self.position = position
+		self.case_y = 1
+		self.niveau = niveau
 		
 
 	def deplacer(self, direction):
 		if direction == "droite":
 			if self.case_x < (nombre_sprite_cote - 1):
-				if self.position.structure[self.case_y][self.case_x + 1] != "m":
+				if self.niveau.structure[self.case_y][self.case_x + 1] != "m":
 					self.case_x += 1
 					self.x = self.case_x * taille_sprite
 				
@@ -61,24 +64,46 @@ class Perso:
 
 		if direction == "gauche":
 			if self.case_x > 0:
-				if self.position.structure[self.case_y][self.case_x - 1] != "m":
+				if self.niveau.structure[self.case_y][self.case_x - 1] != "m":
 					self.case_x -= 1
 					self.x = self.case_x * taille_sprite
 				
 
 		if direction == "haut":
 			if self.case_y > 0:
-				if self.position.structure[self.case_y - 1][self.case_x ] != "m":
-					self.case_y -= 1
-					self.y = self.case_y * taille_sprite
+				if self.niveau.structure[self.case_y - 1][self.case_x ] != "m":
+					if self.niveau.structure[self.case_y - 1][self.case_x] != "x":
+						self.case_y -= 1
+						self.y = self.case_y * taille_sprite
 				
 
 		if direction == "bas":
-			if self.case_y < (nombre_sprite_cote - 1):
-				if self.position.structure[self.case_y + 1][self.case_x] != "m":
+			if self.case_y < (nombre_sprite_cote):
+				if self.niveau.structure[self.case_y + 1][self.case_x] != "m":
 					self.case_y += 1
 					self.y = self.case_y * taille_sprite
 				
+
+
+
+class Objet_aleatoire:  # the class for the items
+    def __init__(self, image_objet, niveau):
+        self.case_y = 0
+        self.case_x = 0
+        self.x = 0
+        self.y = 0
+        self.niveau = niveau
+        self.loaded = True
+        self.image_objet = image_objet
+
+    def afficher(self, image_objet, fenetre):
+        while self.loaded:
+            self.case_x = random.randint(0, 14) 
+            self.case_y = random.randint(0, 14)  
+            if self.niveau.structure[self.case_y][self.case_x] == "0": e
+                self.y = self.case_y * taille_sprite  
+                self.x = self.case_x * taille_sprite
+                self.loaded = False  
 
 
 
