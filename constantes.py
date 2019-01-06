aa9622d75b300a31d073815a53fd1994a2b43057
Malau <malaury.lemaitresalmon@gmail.com>
2019-01-06 16:58:06 +0100

    MAJ constantes

--- constantes.py
+++ constantes.py
@@ -6,13 +6,14 @@ cote_fenetre = nombre_sprite_cote * taille_sprite
 titre_fenetre = "Labyrinthe MacGyver"
 image_icone = "Ressources/MacGyver.png"
 
-image_accueil = "Ressources/accueil.png"
+image_accueil = "Ressources/accueil1.jpg"
 
 image_fond = "Ressources/fond2.jpg"
 
 image_arrivee = "Ressources/Gardien.png"
 image_mur = "Ressources/murs.png"
 image_avatar = "Ressources/MacGyver.png"
-image_aiguille = "Ressources/aiguille1.png"
+image_aiguille = "Ressources/aiguille1.jpg"
 image_ether = "Ressources/ether1.png"
 image_tube = "Ressources/tube_plastique1.png"
+image_fond_fin = "Ressources/fond3.jpg"
